62e5d25ae7bd1f4e4e47843bbfe4dcd6237aa46c
rambhajan mali <rambhajanmali987@gmail.com>
2025-02-04 12:00:20 +0530

    backtracking

---
+++ Devide and conquer/backtracking.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+void printPermutation(string str, int index){
+    //base case
+    if(index >= str.length()){
+        cout<<str<<endl;
+        return;
+    }
+
+    for(int j = index; j<str.length(); j++){
+        swap(str[index], str[j]);
+        //recursive call
+        printPermutation(str, index+1);
+       //backtrack
+        swap(str[index], str[j]);
+    }
+}
+
+int main(){
+    string str = "ABC";
+    int index = 0;  
+    printPermutation(str, index);
+    return 0;
+}

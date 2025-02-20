aab4781d33759ad07a431e71f75de53c0c475066
rambhajan mali <rambhajanmali987@gmail.com>
2025-02-20 22:24:24 +0530

    valid parentheses

--- Stack/valid-parenthesis.cpp
+++ Stack/valid-parenthesis.cpp
@@ -1,49 +1,61 @@
 // class Solution {
 //     public:
-#include<iostream>
-#include<stack>
+#include <iostream>
+#include <stack>
 using namespace std;
 
-        bool isValid(string s) {
-            stack<char> st;
-            for(int i = 0; i<s.length(); i++){
-                char ch = s[i];
-                if(ch == '(' || ch == '{' || ch == '['){
-                    //for open bracket => just push
-                    st.push(ch);
+bool isValid(string s)
+{
+    stack<char> st;
+    for (int i = 0; i < s.length(); i++)
+    {
+        char ch = s[i];
+        if (ch == '(' || ch == '{' || ch == '[')
+        {
+            // for open bracket => just push
+            st.push(ch);
+        }
+        else
+        {
+            // closing bracket
+            if (!st.empty())
+            {
+                if (ch == ')' && st.top() == '(')
+                {
+                    st.pop();
                 }
-                else{
-                    //closing bracket
-                    if(!st.empty()){
-                         if(ch == ')' && st.top() == '('){
-                        st.pop();
-                    }
-                    else if(ch == ']' && st.top() == '['){
-                        st.pop();
-                    }
-                   else if(ch == '}' && st.top() == '{'){
-                        st.pop();
-                    }
-                    else{
-                        //no match
-                        return false;
-                    }
-                    }
-                    else{
-                        return false;
-                    }
-                   
+                else if (ch == ']' && st.top() == '[')
+                {
+                    st.pop();
+                }
+                else if (ch == '}' && st.top() == '{')
+                {
+                    st.pop();
+                }
+                else
+                {
+                    // no match
+                    return false;
                 }
             }
-            if(st.size() == 0){
-                return true;
-            }
-            else{
+            else
+            {
                 return false;
             }
         }
-  int main(){
-        string s = "{[])}";
-        cout<<isValid(s)<<endl;
-        return 0;
-  }
+    }
+    if (st.size() == 0)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+int main()
+{
+    string s = "{[])}";
+    cout << isValid(s) << endl;
+    return 0;
+}
